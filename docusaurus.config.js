38954a9ad05d9f80b0cd570da83ebb2f968a4194
Kimi Gao <mrgaonju@gmail.com>
2019-11-12 00:13:36 +0800

    upgrade(deps): docusaurus to 2.0.0-alpha.34

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,6 +11,7 @@ module.exports = {
   projectName: 'docsite', // Usually your repo name.
   themes: ['@docusaurus/theme-live-codeblock'],
   themeConfig: {
+    disableDarkMode: false,
     navbar: {
       title: githubOrg,
       logo: {
