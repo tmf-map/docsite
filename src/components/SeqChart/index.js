8051bcee94f8b638c6ec15de9a789dd9d49ad946
Kimi Gao <kimi.gao@outlook.com>
2020-01-18 23:18:23 +0800

    use react hooks

--- src/components/SeqChart/index.js
+++ src/components/SeqChart/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import cx from 'classnames';
 import {renderMsc} from 'mscgenjs';
 import styles from './index.module.css';
 
-export default class SeqChart extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      uuid: this.genUuidV4()
-    };
-  }
+function genUuidV4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
-  componentDidMount() {
-    const {
-      inputType = 'msc',
-      mirrorEntitiesOnBottom,
-      additionalTemplate,
-      includeSource
-    } = this.props;
+function handleRenderMscResult(parseError) {
+  if (parseError) {
+    console.error(parseError);
+  }
+}
+
+export default function SeqChart({
+  inputType = 'msc',
+  mirrorEntitiesOnBottom,
+  additionalTemplate,
+  includeSource,
+  children,
+  className
+}) {
+  const [uuid] = useState(genUuidV4());
+
+  useEffect(() => {
     const options = {inputType};
     if (mirrorEntitiesOnBottom) {
       options.mirrorEntitiesOnBottom = mirrorEntitiesOnBottom;
@@ -31,36 +41,15 @@ export default class SeqChart extends React.Component {
 
     renderMsc(
       `${inputType} {
-        ${this.props.children}
+        ${children}
       }`,
       {
-        elementId: this.state.uuid,
+        elementId: uuid,
         ...options
       },
-      this.handleRenderMscResult
+      handleRenderMscResult
     );
-  }
+  });
 
-  genUuidV4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      const r = (Math.random() * 16) | 0;
-      const v = c === 'x' ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    });
-  }
-
-  handleRenderMscResult(parseError) {
-    if (parseError) {
-      console.error(parseError);
-    }
-  }
-
-  render() {
-    return (
-      <div
-        id={this.state.uuid}
-        className={cx(styles['seq-chart'], this.props.className)}
-      />
-    );
-  }
+  return <div id={uuid} className={cx(styles['seq-chart'], className)} />;
 }
