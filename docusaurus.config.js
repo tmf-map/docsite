9b09d180ff6b3720a3517fd0e5d04e3368228e7b
Kimi Gao <kimi.gao@outlook.com>
2020-04-09 13:00:32 +0800

    add(blog): functional programming reading notes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -223,6 +223,10 @@ module.exports = {
             }
           }
         },
+        blog: {
+          path: './blog',
+          editUrl: `https://github.com/${githubOrg}/${githubRepo}/edit/master/`
+        },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
         }
