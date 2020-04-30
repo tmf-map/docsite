7b4617013658abdc99487f3298e1cbb7a5a0f325
robbie <38212028+Robbie-Han@users.noreply.github.com>
2020-04-30 14:35:08 +0800

    add(webpack): add loaders and plugins (#191)
    * add(webpack): add loaders and plugins
    
    * optimize content of loaders
    
    * add plugins
    
    * optimize content of loaders and plugins
    
    * optimize content of plugins
    
    * optimize plugins
    
    * optimize content of plugins
    
    * add tip to css loader
    
    Co-authored-by: robbie <38212028+USTC-Han@users.noreply.github.com>

--- sidebars/webpack.js
+++ sidebars/webpack.js
@@ -1,5 +1,9 @@
 module.exports = {
-  '1.Basic': ['webpack/1.Basic/basic'],
+  '1.Basic': [
+    'webpack/1.Basic/basic',
+    'webpack/1.Basic/loaders',
+    'webpack/1.Basic/plugins'
+  ],
   '2.Advanced': [
     'webpack/2.Advanced/resource-extraction',
     'webpack/2.Advanced/code-split',
