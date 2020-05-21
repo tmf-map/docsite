4131bdfd250b42f5873fe1603277dd06dbd546df
Goroyal <dgq.cs.se@gmail.com>
2020-05-21 11:27:15 +0800

    add(backend): create docs for distributed system and cache

---
+++ sidebars/cache.js
@@ -0,0 +1,4 @@
+module.exports = {
+    本机缓存: ['cache/local-cache/ehcache'],
+    远程缓存: ['cache/remote-cache/redis']
+};
---
+++ sidebars/distributed-system.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'CAP 理论': [
+        'distributed-system/CAP/introduce-CAP',
+        'distributed-system/CAP/high-availability',
+        'distributed-system/CAP/consistency'
+    ]
+};
