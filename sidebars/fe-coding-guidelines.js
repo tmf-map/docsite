c824a1ccc5292d662d8e2eebe33c73f2c5a72fd5
Kimi Gao <kimi.gao@outlook.com>
2020-05-27 00:17:57 +0800

    add maintainability 1

--- sidebars/fe-coding-guidelines.js
+++ sidebars/fe-coding-guidelines.js
@@ -3,7 +3,12 @@ module.exports = [
   {
     '2. Maintainability': [
       'fe-coding-guidelines/2.maintainability/naming',
-      'fe-coding-guidelines/2.maintainability/variable'
+      'fe-coding-guidelines/2.maintainability/variable',
+      'fe-coding-guidelines/2.maintainability/comment',
+      'fe-coding-guidelines/2.maintainability/logic-operator',
+      'fe-coding-guidelines/2.maintainability/function',
+      'fe-coding-guidelines/2.maintainability/other'
     ]
-  }
+  },
+  'fe-coding-guidelines/3.strictness'
 ];
