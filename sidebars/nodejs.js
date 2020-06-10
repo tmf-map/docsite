300c9275941353126e5d94d13834d0d0631e43ae
Kimi Gao <kimi.gao@outlook.com>
2020-06-10 22:18:24 +0800

    add(nodejs): intro to npm

--- sidebars/nodejs.js
+++ sidebars/nodejs.js
@@ -2,6 +2,7 @@ module.exports = [
   {
     'Package Manager': [
       'nodejs/package-manager/package-version',
+      'nodejs/package-manager/npm',
       'nodejs/package-manager/yarn',
       'nodejs/package-manager/npm-vs-yarn'
     ]
