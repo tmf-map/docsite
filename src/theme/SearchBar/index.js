efe354e9b255b360574ece25a08d16da08b14374
Kimi Gao <kimi.gao@outlook.com>
2020-07-02 23:50:18 +0800

    upgrade theme SearchBar

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -5,32 +5,29 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, {useState, useRef, useCallback} from 'react';
-import classnames from 'classnames';
-import {useHistory} from '@docusaurus/router';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './styles.module.css';
+import React, { useRef, useCallback } from "react";
+import classnames from "classnames";
+import { useHistory } from "@docusaurus/router";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 const Search = props => {
-  const [algoliaLoaded, setAlgoliaLoaded] = useState(false);
   const initialized = useRef(false);
   const searchBarRef = useRef(null);
   const history = useHistory();
-  const {siteConfig = {}} = useDocusaurusContext();
-  const {baseUrl} = siteConfig;
-  const initAlgolia = () => {
-    if (!initialized.current) {
-      // eslint-disable-next-line
-      new window.DocSearch({
-        searchData: window.searchData,
-        inputSelector: '#search_input_react',
+  const { siteConfig = {} } = useDocusaurusContext();
+  const { baseUrl } = siteConfig;
+  const initAlgolia = (searchDocs, searchIndex, DocSearch) => {
+      new DocSearch({
+        searchDocs,
+        searchIndex,
+        inputSelector: "#search_input_react",
         // Override algolia's default selection event, allowing us to do client-side
         // navigation and avoiding a full page refresh.
         handleSelected: (_input, _event, suggestion) => {
           const url = baseUrl + suggestion.url;
           // Use an anchor tag to parse the absolute url into a relative url
           // Alternatively, we can use new URL(suggestion.url) but its not supported in IE
-          const a = document.createElement('a');
+          const a = document.createElement("a");
           a.href = url;
           // Algolia use closest parent element id #__docusaurus when a h1 page title does not have an id
           // So, we can safely remove it. See https://github.com/facebook/docusaurus/issues/1828 for more details.
@@ -38,80 +35,71 @@ const Search = props => {
           history.push(url);
         }
       });
+  };
+
+  const getSearchDoc = () =>
+    process.env.NODE_ENV === "production"
+      ? fetch(`${baseUrl}search-doc.json`).then((content) => content.json())
+      : Promise.resolve([]);
+
+  const getLunrIndex = () =>
+    process.env.NODE_ENV === "production"
+      ? fetch(`${baseUrl}lunr-index.json`).then((content) => content.json())
+      : Promise.resolve([]);
+
+  const loadAlgolia = () => {
+    if (!initialized.current) {
+      Promise.all([
+        getSearchDoc(),
+        getLunrIndex(),
+        import("./lib/DocSearch"),
+        import("./algolia.css")
+      ]).then(([searchDocs, searchIndex, { default: DocSearch }]) => {
+        initAlgolia(searchDocs, searchIndex, DocSearch);
+      });
       initialized.current = true;
     }
   };
 
-  const getSearchData = () =>
-    process.env.NODE_ENV === 'production'
-      ? fetch(`${baseUrl}search-data.json`).then(content => content.json())
-      : Promise.resolve([]);
+  const toggleSearchIconClick = useCallback(
+    e => {
+      if (!searchBarRef.current.contains(e.target)) {
+        searchBarRef.current.focus();
+      }
 
-  const loadAlgolia = () => {
-    if (!algoliaLoaded) {
-      Promise.all([
-        getSearchData(),
-        import('./lib/DocSearch'),
-        import('./algolia.css')
-      ]).then(([searchData, {default: DocSearch}]) => {
-        setAlgoliaLoaded(true);
-        window.searchData = searchData;
-        window.DocSearch = DocSearch;
-        initAlgolia();
-      });
-    } else {
-      initAlgolia();
-    }
-  };
-
-  const toggleSearchInput = useCallback(() => {
-    loadAlgolia();
-
-    if (algoliaLoaded) {
-      searchBarRef.current.focus();
-    }
-
-    props.handleSearchBarToggle(!props.isSearchBarExpanded);
-  }, [props.isSearchBarExpanded]);
-
-  const handleSearchInputBlur = useCallback(() => {
-    props.handleSearchBarToggle(!props.isSearchBarExpanded);
-  }, [props.isSearchBarExpanded]);
-
-  const handleSearchInput = useCallback(e => {
-    const needFocus = e.type !== 'mouseover';
-
-    loadAlgolia(needFocus);
-  });
+      props.handleSearchBarToggle(!props.isSearchBarExpanded);
+    },
+    [props.isSearchBarExpanded]
+  );
 
   return (
     <div className="navbar__search" key="search-box">
-      <div className={styles.searchWrapper}>
-        <span
-          aria-label="expand searchbar"
-          role="button"
-          className={classnames(styles.searchIconButton, {
-            [styles.searchIconButtonHidden]: props.isSearchBarExpanded
-          })}
-          onClick={toggleSearchInput}
-          onKeyDown={toggleSearchInput}
-          tabIndex={0}
-        />
-
-        <input
-          id="search_input_react"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-          className={classnames('navbar__search-input', styles.searchInput, {
-            [styles.searchInputExpanded]: props.isSearchBarExpanded
-          })}
-          onMouseOver={handleSearchInput}
-          onFocus={handleSearchInput}
-          onBlur={handleSearchInputBlur}
-          ref={searchBarRef}
-        />
-      </div>
+      <span
+        aria-label="expand searchbar"
+        role="button"
+        className={classnames("search-icon", {
+          "search-icon-hidden": props.isSearchBarExpanded
+        })}
+        onClick={toggleSearchIconClick}
+        onKeyDown={toggleSearchIconClick}
+        tabIndex={0}
+      />
+      <input
+        id="search_input_react"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+        className={classnames(
+          "navbar__search-input",
+          { "search-bar-expanded": props.isSearchBarExpanded },
+          { "search-bar": !props.isSearchBarExpanded }
+        )}
+        onClick={loadAlgolia}
+        onMouseOver={loadAlgolia}
+        onFocus={toggleSearchIconClick}
+        onBlur={toggleSearchIconClick}
+        ref={searchBarRef}
+      />
     </div>
   );
 };
