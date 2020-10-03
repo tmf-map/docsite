c0f14e334d152d484e16f8d085f394c9dcdec681
Kimi Gao <kimi.gao@outlook.com>
2020-10-04 01:38:02 +0800

    add basic types in python

--- sidebars/python.js
+++ sidebars/python.js
@@ -4,6 +4,14 @@ module.exports = [
       'python/1.get-started/history',
       'python/1.get-started/installation',
       'python/1.get-started/ide-tools'
+    ],
+    '2. Basic Syntax': [
+      'python/2.basic-syntax/data-type',
+      'python/2.basic-syntax/variable',
+      'python/2.basic-syntax/if-else',
+      'python/2.basic-syntax/loop',
+      'python/2.basic-syntax/list',
+      'python/2.basic-syntax/dict'
     ]
   }
 ];
