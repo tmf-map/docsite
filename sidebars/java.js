3f2dc3e172ca3bcfc23ce8c986cee2489831d9d6
Goroyal <dgq.cs.se@gmail.com>
2020-12-31 23:00:21 +0800

    Fix document name reference

--- sidebars/java.js
+++ sidebars/java.js
@@ -1,6 +1,6 @@
 module.exports = {
   面向对象编程: [
     'java/object-oriented-programming/abstract-class-interface',
-    'java/object-oriented-programming/ooo-feature'
+    'java/object-oriented-programming/ooo-features'
   ]
 };
