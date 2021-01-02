17f6b9019179675da45f5f7867a7c59172df395d
Goroyal Dai <dgq.cs.se@gmail.com>
2021-01-02 10:24:16 +0800

    add(java): doc for introducing OOO features (#459)
    * add(java): doc for introducing OOO features
    
    * Fix document name reference
    
    * Java to java in code block
    
    * add additionalLanguages
    
    Co-authored-by: Kimi Gao <kimi.gao@outlook.com>

--- sidebars/java.js
+++ sidebars/java.js
@@ -1,3 +1,6 @@
 module.exports = {
-  面向对象编程: ['java/object-oriented-programming/abstract-class-interface']
+  面向对象编程: [
+    'java/object-oriented-programming/abstract-class-interface',
+    'java/object-oriented-programming/ooo-features'
+  ]
 };
