5ed5f202921376aa776aaf091119f85a385326d4
Kimi <kimi.gao@outlook.com>
2021-06-19 13:16:03 +0800

    revert white header

--- src/theme/Toggle/index.js
+++ src/theme/Toggle/index.js
@@ -14,7 +14,7 @@ import classnames from 'classnames';
 import styles from './styles.module.css';
 
 const Moon = () => <span className={classnames(styles.toggle, styles.moon)} />;
-// const Sun = () => <span className={classnames(styles.toggle, styles.sun)} />;
+const Sun = () => <span className={classnames(styles.toggle, styles.sun)} />;
 
 export default function (props) {
   const {isClient} = useDocusaurusContext();
@@ -23,8 +23,7 @@ export default function (props) {
       disabled={!isClient}
       icons={{
         checked: <Moon />,
-        // unchecked: <Sun />,
-        unchecked: <Moon />
+        unchecked: <Sun />
       }}
       {...props}
     />
